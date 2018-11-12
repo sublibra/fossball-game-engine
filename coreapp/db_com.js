9483cb842d50f6d940c51b0d2fd8fad82087d910
Jonas Hörström <jonas.horstrom@gmail.com>
2018-11-12 09:42:17 +0100

    Start of engine connection

---
+++ coreapp/db_com.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+const configuration = require('../config.js');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
